73471fb5f4b11c3b49cbc622ad8fa86f45afc285
LindaOuer <linda.ouerfelli@gmail.com>
2023-11-20 16:25:03 +0100

    Models - Event

---
+++ events/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Event(models.Model):
+    CATEGORY_CHOICES = (
+        ('Music', 'Music'),
+        ('Cinema', 'Cinema'),
+        ('Sport', 'Sport'),
+    )
+
+    title = models.CharField(max_length=255, null=True)
+    description = models.TextField()
+    eventImage = models.ImageField()
+
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=8)
+    state = models.BooleanField(default=False)
+    nbrParticipants = models.IntegerField(default=0)
+    eventDate = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
